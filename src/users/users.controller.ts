28a1b7bc0546279b10ab47308b6cf0e28856e224
MahidunNobi <mdmahidunnobi@gmail.com>
2025-03-02 09:39:43 +0600

    CRUD: users & episodes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll() {
+    return 'all the users are here';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  createUser(@Body() body: Record<string, string>) {
+    return body;
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id') id: string,
+    @Body() updatedUser: Record<string, string>,
+  ) {
+    return { id, updatedUser };
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return { id };
+  }
+}
