e8cc73efbef8a09b907b667803e0944befd37f94
MahidunNobi <mdmahidunnobi@gmail.com>
2025-03-05 17:33:13 +0600

    Add-Validation: Not found

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
@@ -39,13 +39,17 @@ export class UsersService {
 
   findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
     if (role) {
-      return this.users.filter((user) => user.role === role);
+      const filteredUsers = this.users.filter((user) => user.role === role);
+      if (!filteredUsers.length) throw new NotFoundException('Role not found');
+      return filteredUsers;
     }
     return this.users;
   }
 
   findOne(id: number) {
-    return this.users.find((user) => user.id === id);
+    const user = this.users.find((user) => user.id === id);
+    if (!user) throw new NotFoundException('User Not Found');
+    return user;
   }
 
   createUser(createUserDto: CreateUserDto) {
