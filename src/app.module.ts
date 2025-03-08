2f884b247e1cf744c84eaa08570510f217d40778
MahidunNobi <mdmahidunnobi@gmail.com>
2025-03-08 16:52:39 +0600

    Added: Database with Prisma ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { EpisodesModule } from './episodes/episodes.module';
 import { TopicsModule } from './topics/topics.module';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [EpisodesModule, TopicsModule, UsersModule],
+  imports: [EpisodesModule, TopicsModule, UsersModule, DatabaseModule, EmployeeModule],
   controllers: [AppController],
   providers: [AppService],
 })
