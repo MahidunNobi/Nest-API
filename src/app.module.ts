80d3ff561bc088d7dce39ee29053f50b428bb447
MahidunNobi <mdmahidunnobi@gmail.com>
2025-03-09 14:38:02 +0600

    Update: Only 5 request can be accepted in every 1 min

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,30 @@ import { TopicsModule } from './topics/topics.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeeModule } from './employee/employee.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [EpisodesModule, TopicsModule, UsersModule, DatabaseModule, EmployeeModule],
+  imports: [
+    EpisodesModule,
+    TopicsModule,
+    UsersModule,
+    DatabaseModule,
+    EmployeeModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 5,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
